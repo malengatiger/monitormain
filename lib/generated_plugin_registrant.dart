30963e39c703c1ace2b1fa2c96e0e4a0ed8b02f6
malengatiger <malengatiger@gmail.com>
2020-11-24 01:04:00 +0200

    🦠  the mountain is being climbed!

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -8,9 +8,11 @@ import 'dart:ui';
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:import_js_library/import_js_library.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:video_player_web/video_player_web.dart';
+import 'package:wakelock_web/wakelock_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -19,8 +21,10 @@ void registerPlugins(PluginRegistry registry) {
   FirebaseFirestoreWeb.registerWith(registry.registrarFor(FirebaseFirestoreWeb));
   FirebaseAuthWeb.registerWith(registry.registrarFor(FirebaseAuthWeb));
   FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
+  ImportJsLibrary.registerWith(registry.registrarFor(ImportJsLibrary));
   SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
   UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
   VideoPlayerPlugin.registerWith(registry.registrarFor(VideoPlayerPlugin));
+  WakelockWeb.registerWith(registry.registrarFor(WakelockWeb));
   registry.registerMessageHandler();
 }
